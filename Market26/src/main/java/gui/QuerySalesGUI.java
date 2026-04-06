4b54c53419738e53bf6a05066f0cb634e91e18f4
i7089 <81593254+i7089@users.noreply.github.com>
2026-04-06 14:06:54 -0400

    Hecha la parte de query sales

--- Market26/src/main/java/gui/QuerySalesGUI.java
+++ Market26/src/main/java/gui/QuerySalesGUI.java
@@ -36,6 +36,11 @@ public class QuerySalesGUI extends JFrame {
 
 	};
 	private JTextField jTextFieldSearch;
+	//Diogo Agregado nuevos fields
+	private final JTextField textFieldPrecioMax = new JTextField();
+	private final JTextField textFieldFecha = new JTextField();
+	private final JLabel label_1 = new JLabel("Precio Maximo");
+	private final JLabel label_2 = new JLabel("Fecha");
 	
 
 	public QuerySalesGUI() {
@@ -80,7 +85,7 @@ public class QuerySalesGUI extends JFrame {
 		this.getContentPane().add(scrollPanelProducts, null);
 		
 		jTextFieldSearch = new JTextField();
-		jTextFieldSearch.setBounds(52, 56, 357, 26);
+		jTextFieldSearch.setBounds(52, 56, 158, 26);
 		getContentPane().add(jTextFieldSearch);
 		jTextFieldSearch.setColumns(10);
 		
@@ -93,24 +98,89 @@ public class QuerySalesGUI extends JFrame {
 					BLFacade facade = MainGUI.getBusinessLogic();
 					Date today = UtilDate.trim(new Date());
 
+					//Diogo - parte para hacer el filtro de date
+					//Los filtros son 1-la del nombre la default 2- De fechas despues de X  3- De precios menores que X 
+					//Comprobar que no es null y puedo hacer el filtro
+					Boolean fecha = false;
+					Boolean precio = false;
+					float precioMax = 0;
+					Date date = null;
+					//Parte del filtro de fecha
+					SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
+					String dateInString = textFieldFecha.getText();
+					if(!dateInString.equals("")) {
+						try {
+							date = formatter.parse(dateInString);
+							fecha = true;
+						}catch(Exception pp) {
+							fecha = false;
+						}
+					}
+					//Parte del filtro de precio 
+					if(!textFieldPrecioMax.getText().equals("")) {
+						try {
+							precioMax = Float.parseFloat(textFieldPrecioMax.getText());
+							precio = true;
+						}catch(Exception pp) {
+							precioMax = 0; 
+							precio = false;
+						}
+					}
+					
+				    //No he tocado esto
 					List<domain.Sale> sales=facade.getPublishedSales(jTextFieldSearch.getText(),today);
 
 					if (sales.isEmpty() ) jLabelProducts.setText(ResourceBundle.getBundle("Etiquetas").getString("QuerySalesGUI.NoProducts"));
 					else jLabelProducts.setText(ResourceBundle.getBundle("Etiquetas").getString("QuerySalesGUI.Products"));
 					for (domain.Sale sale:sales){
+						//Diogo Parte del filtro
 						if(!sale.getSeller().getName().equals("Basura")) {
-							Vector<Object> row = new Vector<Object>();
-							row.add(sale.getTitle());
-							row.add(sale.getPrice());
-							row.add(new SimpleDateFormat("dd-MM-yyyy").format(sale.getPublicationDate()));
-							row.add(sale); // product object added in order to obtain it with tableModelProducts.getValueAt(i,2)
-							tableModelProducts.addRow(row);		
+							//Si se ha dado ambas la fecha y el precio
+							if( fecha && precio) {
+								if(sale.getPrice()<precioMax &&sale.getPublicationDate().after(date)) {
+									Vector<Object> row = new Vector<Object>();
+									row.add(sale.getTitle());
+									row.add(sale.getPrice());
+									row.add(new SimpleDateFormat("dd-MM-yyyy").format(sale.getPublicationDate()));
+									row.add(sale); // product object added in order to obtain it with tableModelProducts.getValueAt(i,2)
+									tableModelProducts.addRow(row);	
+								}
+						    //Filtro condicion para agregar dependiendo de fecha
+							}else if(fecha) {
+								if(sale.getPublicationDate().after(date)) {
+									Vector<Object> row = new Vector<Object>();
+									row.add(sale.getTitle());
+									row.add(sale.getPrice());
+									row.add(new SimpleDateFormat("dd-MM-yyyy").format(sale.getPublicationDate()));
+									row.add(sale); // product object added in order to obtain it with tableModelProducts.getValueAt(i,2)
+									tableModelProducts.addRow(row);	
+								}
+							//Solo precio 
+							}else if(precio){
+								if(sale.getPrice()<precioMax) {
+									Vector<Object> row = new Vector<Object>();
+									row.add(sale.getTitle());
+									row.add(sale.getPrice());
+									row.add(new SimpleDateFormat("dd-MM-yyyy").format(sale.getPublicationDate()));
+									row.add(sale); // product object added in order to obtain it with tableModelProducts.getValueAt(i,2)
+									tableModelProducts.addRow(row);	
+								}
+							//Default
+							}else {
+								Vector<Object> row = new Vector<Object>();
+								row.add(sale.getTitle());
+								row.add(sale.getPrice());
+								row.add(new SimpleDateFormat("dd-MM-yyyy").format(sale.getPublicationDate()));
+								row.add(sale); // product object added in order to obtain it with tableModelProducts.getValueAt(i,2)
+								tableModelProducts.addRow(row);	
+							}
 						}
 					}
 				} catch (Exception e1) {
 
 					e1.printStackTrace();
 				}
+		 		
 				tableProducts.getColumnModel().getColumn(0).setPreferredWidth(200);
 				tableProducts.getColumnModel().getColumn(1).setPreferredWidth(10);
 				tableProducts.getColumnModel().getColumn(1).setPreferredWidth(70);
@@ -119,8 +189,26 @@ public class QuerySalesGUI extends JFrame {
 		 		
 		 	}
 		 });
-		jButtonSearch.setBounds(427, 56, 117, 29);
+		jButtonSearch.setBounds(549, 54, 117, 29);
 		getContentPane().add(jButtonSearch);
+		textFieldPrecioMax.setColumns(10);
+		textFieldPrecioMax.setBounds(220, 56, 130, 26);
+		
+		getContentPane().add(textFieldPrecioMax);
+		textFieldFecha.setColumns(10);
+		textFieldFecha.setBounds(362, 56, 130, 26);
+		
+		getContentPane().add(textFieldFecha);
+		
+		JLabel label = new JLabel("Nombre");
+		label.setBounds(52, 27, 60, 17);
+		getContentPane().add(label);
+		label_1.setBounds(220, 27, 60, 17);
+		
+		getContentPane().add(label_1);
+		label_2.setBounds(368, 27, 60, 17);
+		
+		getContentPane().add(label_2);
 		
 	    
 		tableProducts.addMouseListener(new MouseAdapter() {
@@ -138,4 +226,3 @@ public class QuerySalesGUI extends JFrame {
 		        }
 		 });
 	}
-}
